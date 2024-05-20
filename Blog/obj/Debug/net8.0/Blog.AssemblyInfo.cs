3f104a96ffe7978d19b6d7bd42793e6fcd296ddc
wbahia <wbahia@gmail.com>
2024-05-20 16:07:01 -0300

    WYSIWYG - Form Blog Post

--- Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ Blog/obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d3574f44e0ae4b3e74802578e6d10f9c6baf8d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+503ececfd3184f176e771b50b613cbc8f10825ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
